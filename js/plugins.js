1d39faa243d169d7d89dd675785d31637cccfe50
BigBlueHat <byoung@bigbluehat.com>
2015-02-23 14:43:30 -0500

    Add license information to plugins list

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,25 @@
 (function ($, undefined) {
 
+    function licenseAnchor(license) {
+        function a(license) {
+            return '<a href="http://spdx.org/licenses/' + license + '">' + license + '</a>';
+        }
+
+        if (license) {
+            if ((list = license.split(' or ')).length > 1) {
+                var output = []
+                $.each(list, function(i, license) {
+                    output.push(a(license));
+                });
+                return output.join(' or ');
+            } else {
+                return a(license);
+            }
+        } else {
+            return license;
+        }
+    }
+
     //Grab plugins.json file
     var plugins = $.getJSON('plugins.json', function(data){
 
@@ -9,6 +29,7 @@
             html += '<td class="name"><a href="' + item.url + '" target="_blank">' + item.name + '</a></td>';
             html += '<td class="description">' + item.description + '</td>';
             html += '<td class="author">' + item.owner + '</td>';
+            html += '<td class="license">' + licenseAnchor(item.license || '') + '</td>';
             $('tbody.pluginList').append(html);
         });
 
