b922e57745ae18d29af47b840f5b5da07ea3249c
BigBlueHat <byoung@bigbluehat.com>
2015-05-20 14:22:34 -0400

    Pretty tags

--- js/plugins.js
+++ js/plugins.js
@@ -29,7 +29,11 @@
             html += '<td><a class="name" href="' + item.url + '" target="_blank">' + item.name + '</a>';
             html += '<br /><span class="text-muted owner">' + item.owner + '</span>';
             html += '<br /><span class="description">' + item.description + '</span></td>';
-            html += '<td class="tags">' + (undefined != item.tags ? item.tags.join(', ') : '') + '</td>';
+            html += '<td class="tags">' + (undefined != item.tags ?
+                item.tags.map(function(tag) {
+                  return '<span class="label label-info tag-' + tag.toLowerCase() + '">' + tag + '</span>';
+                }).join(' ')
+                : '') + '</td>';
             html += '<td class="license">' + licenseAnchor(item.license || '') + '</td>';
             html += '<td class="for">' + (undefined != item.for ? item.for.join(', ') : '') + '</td>';
             $('tbody.pluginList').append(html);
